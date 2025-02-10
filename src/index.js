471385ffd0982d33f25cc57879f8a1672c55e59f
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-02-10 14:47:13 -0600

    Update the nav bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import App from "./app";
 import "./styles.css";
 
+/**
+ * Updates the DOM to reflect the app state
+ * @param {*} app 
+ */
+function updateDisplay(app) {
+    const navigationList = document.getElementById("navigation-list");
+    navigationList.textContent = "";
+    let projects = app.getProjectList();
+    console.log(projects);
+    for (const project of projects) {
+        console.log(project);
+        const projectTag = document.createElement("p");
+        projectTag.textContent = project;
+        navigationList.appendChild(projectTag);
+    }
+}
+
 let app = new App();
-console.log(app.getProjectState(0));
-console.log(app.getProjectList());
+updateDisplay(app);
