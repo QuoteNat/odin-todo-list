6acfdd38cd43a4d49f815bd553c8a9ef354a96db
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-02-10 15:08:01 -0600

    Update the todo list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import App from "./app";
 import "./styles.css";
+import { format } from 'date-fns';
+
+let currentProject = 0;
 
 /**
  * Updates the DOM to reflect the app state
@@ -7,7 +10,10 @@ import "./styles.css";
  */
 function updateDisplay(app) {
     const navigationList = document.getElementById("navigation-list");
+    const todolistDiv = document.getElementById("todos");
     navigationList.textContent = "";
+    todolistDiv.textContent = "";
+
     let projects = app.getProjectList();
     console.log(projects);
     for (const project of projects) {
@@ -15,6 +21,24 @@ function updateDisplay(app) {
         const projectTag = document.createElement("p");
         projectTag.textContent = project;
         navigationList.appendChild(projectTag);
+    }
+
+    let projectTodos = app.getProjectState(currentProject);
+    console.log(projectTodos.todos);
+    for (const todo of projectTodos.todos) {
+        const todoDiv = document.createElement("div");
+        todoDiv.className = "todo";
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        const name = document.createElement("p");
+        name.textContent = todo.title;
+        const date = document.createElement("p");
+        date.textContent = format(todo.dueDate, "MM/dd/yyyy")
+
+        todoDiv.appendChild(checkbox);
+        todoDiv.appendChild(name);
+        todoDiv.appendChild(date);
+        todolistDiv.appendChild(todoDiv);
     }
 }
 
